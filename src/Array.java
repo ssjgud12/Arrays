07ea9d6b7b7873a35ca238918eae0e0ba4921fc7
ALVINOLAYEMI-STUDENT <G00428825@atu.ie>
2024-10-14 10:09:44 +0100

    Created Array repo

---
+++ src/Array.java
@@ -0,0 +1,27 @@
+public class Array {
+    public static void main(String[] args) {
+
+       int[] anArray = new int [10];
+       anArray [0] = 100;
+       anArray [1] = 200;
+       anArray [2] = 300;
+       anArray [3] = 400;
+       anArray [4] = 500;
+       anArray [5] = 600;
+       anArray [6] = 700;
+       anArray [7] = 800;
+       anArray [8] = 900;
+       anArray [9] = 1000;
+
+       for (int i =0; i < anArray.length; i++)
+       {
+           System.out.println(anArray[i]);
+       }
+
+
+
+
+
+
+    }
+}
